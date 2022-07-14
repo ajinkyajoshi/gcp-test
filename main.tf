bee44212105b8f8abafcf9a0fc15bb1e764eaeca
Ajinkya Joshi <ajinkya.joshi.d@gmail.com>
2022-07-14 17:17:22 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "google_monitoring_alert_policy" "alert_policy" {
+  project = "terraform-350522"
+  for_each = var.monitoring_alert_policy
+    display_name = each.value.display_name
+    combiner     = each.value.combiner
+    conditions {
+      display_name = each.value.condition_display_name
+      condition_threshold {
+        filter     = each.value.filter
+        duration   = each.value.duration
+        comparison = each.value.comparison
+        aggregations {
+          alignment_period   = each.value.alignment_period
+          per_series_aligner = each.value.per_series_aligner
+        }
+      }
+    }
+
+  user_labels = {
+    foo = "bar"
+  }
+}

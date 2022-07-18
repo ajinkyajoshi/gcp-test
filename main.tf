dea922c88a189c9a00e3c759714a44ad6ef0f272
Ajinkya Joshi <ajinkya.joshi.d@gmail.com>
2022-07-18 16:25:38 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "google_monitoring_alert_policy" "alert_policy" {
 
 */
 
-resource "google_monitoring_alert_policy" "alert_policy" {
+/*resource "google_monitoring_alert_policy" "alert_policy" {
   project = "xyz"
   for_each = var.monitoring_alert_policy
     display_name = each.value.display_name
@@ -54,4 +54,39 @@ resource "google_monitoring_alert_policy" "alert_policy" {
  documentation {
   content = "telecom-interconnect-alert | P1 | DC Traffic Check"
  }
+}
+*/
+    
+resource "google_monitoring_alert_policy" "alert_policy" {
+  project = "xyz"
+ # notification_channels = [resource.google_monitoring_notification_channel.snow-notification.name]
+  for_each = var.monitoring_alert_policy
+    display_name = each.value.display_name
+    combiner     = each.value.combiner
+    conditions {
+      display_name = each.value.condition_display_name
+      condition_threshold {
+        filter     = each.value.filter
+        duration   = each.value.duration
+        comparison = each.value.comparison
+          aggregations {
+           alignment_period   = each.value.alignment_period
+           per_series_aligner = each.value.per_series_aligner
+           cross_series_reducer=try([each.value.cross_series_reducer],[])
+          }
+     
+            trigger {
+            count = each.value.trigger_count
+            percent = try([each.value.percent],[])
+            }
+      } 
+    }
+    user_labels = {
+     severity = each.value.severity
+     resourcetype = each.value.resource_type
+    }
+
+    documentation {
+      content = "telecom-interconnect-alert | P1 | DC Traffic Check"
+    }
 }

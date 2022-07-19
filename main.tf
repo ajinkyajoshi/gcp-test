05d537c66a293d4e93bb8fc7171a866c47e20a62
Ajinkya Joshi <ajinkya.joshi.d@gmail.com>
2022-07-19 10:46:55 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,24 +4,21 @@ locals {
       display_name = policy.display_name
       combiner     = policy.combiner
       conditions   = policy.conditions
-      #enabled               = lookup(policy, "enabled", true)
-      #notification_channels = lookup(policy, "notification_channels", [])
+      notification_channels = lookup(policy, "notification_channels", [])
       user_labels = lookup(policy, "user_labels", null)
-      #documentation         = lookup(policy, "documentation", null)
+      documentation = lookup(policy, "documentation", null)
     }
   ]
 }
-
-
 
 resource "google_monitoring_alert_policy" "policies" {
   for_each     = { for policy in var.policies : lower(policy.display_name) => policy }
   display_name = each.value.display_name
   project      = "xyz"
   combiner     = each.value.combiner
-  #enabled               = each.value.enabled
-  #notification_channels = each.value.notification_channels
+  notification_channels = each.value.notification_channels
   user_labels = each.value.user_labels
+  #documentation= each.value.documentation
 
 
   conditions {
@@ -34,14 +31,12 @@ resource "google_monitoring_alert_policy" "policies" {
         duration           = lookup(condition_threshold.value, "duration", null)
         comparison         = lookup(condition_threshold.value, "comparison", null)
         threshold_value    = lookup(condition_threshold.value, "threshold_value", null)
-        denominator_filter = lookup(condition_threshold.value, "denominator_filter", null)
         filter             = lookup(condition_threshold.value, "filter", null)
 
         dynamic "aggregations" {
           for_each = lookup(condition_threshold.value, "aggregations", null) == null ? [] : [condition_threshold.value.aggregations]
           content {
             per_series_aligner   = lookup(aggregations.value, "per_series_aligner", null)
-            group_by_fields      = lookup(aggregations.value, "group_by_fields", [])
             alignment_period     = lookup(aggregations.value, "alignment_period", null)
             cross_series_reducer = lookup(aggregations.value, "cross_series_reducer", null)
           }
@@ -55,9 +50,6 @@ resource "google_monitoring_alert_policy" "policies" {
         }
       }
     }
-
-
-
     dynamic "condition_absent" {
       for_each = lookup(each.value.conditions, "condition_absent", null) == null ? [] : [each.value.conditions.condition_absent]
       content {
@@ -67,7 +59,6 @@ resource "google_monitoring_alert_policy" "policies" {
           for_each = lookup(condition_absent.value, "aggregations", null) == null ? [] : [condition_absent.value.aggregations]
           content {
             per_series_aligner   = lookup(aggregations.value, "per_series_aligner", null)
-            group_by_fields      = lookup(aggregations.value, "group_by_fields", [])
             alignment_period     = lookup(aggregations.value, "alignment_period", null)
             cross_series_reducer = lookup(aggregations.value, "cross_series_reducer", null)
           }
@@ -80,6 +71,12 @@ resource "google_monitoring_alert_policy" "policies" {
           }
         }
       }
+    }
+  }
+  dynamic "documentation" {
+    for_each = var.documentation
+    content {
+      content = documentation.value["content"]
     }
   }
 }
